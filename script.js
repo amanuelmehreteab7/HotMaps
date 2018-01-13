68defe0db14a37bccaf614bcc593d8db2a325a1d
mjsimon91 <mjsimon91@gmail.com>
2018-01-13 11:18:35 -0500

    Search is now working and providing coordinates on the map

--- script.js
+++ script.js
@@ -2,7 +2,36 @@ var api_key = 'AIzaSyALhVNfaKgpvJuLqX6VuPljcwgUcEj_qHw'
 var searchLocation;
 var searchLatitude;
 var searchLongitude;
+var coordinates;
 
+
+
+
+function initMap() {
+  var uluru = {
+    lat: coordinates.lat,
+    lng: coordinates.lng
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+function placeMarker(lat, long) {
+  var uluru = {
+    lat: lat,
+    long: long
+  }
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
 
 // Get the value of the search term
 $("#search").keypress(function(e) {
@@ -16,9 +45,9 @@ $("#search").keypress(function(e) {
         // initMap();
     }
 });
-
-//Need to convert the search term into latitude and longitude in order to find the center of the Maps
-
+//
+// //Need to convert the search term into latitude and longitude in order to find the center of the Maps
+//
 function searchCoordinates (){
   var coordinatesQueryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + searchLocation + '&key=' + api_key;
 
@@ -29,21 +58,31 @@ function searchCoordinates (){
     console.log('query url ' + coordinatesQueryURL);
     console.log(response);
     for (var i = 0; i < response.results.length; i++) {
-      searchLatitude = response.results[i].geometry.location.lat;
-      searchLongitude = response.results[i].geometry.location.lng;
-      initMap();
+      searchLatitude = parseFloat(response.results[0].geometry.location.lat)
+      searchLongitude = parseFloat(response.results[0].geometry.location.lng)
+      coordinates = {
+            lat: searchLatitude,
+            lng: searchLongitude
+          };
+          console.log(coordinates);
     }
+    initMap();
+    // placeMarker(coordinates.lat,coordinates.lng);
   });
 }
-
-function initMap(){
-  var coordinates = {
-    lat: searchLatitude,
-    lng: searchLongitude
-  };
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-         zoom: 4,
-         center: coordinates
-       });
-}
+//
+// $(document).ready(function(){
+//   function initMap(){
+//     var coordinates = {
+//       lat: 38.9071923,
+//       lng: searchLongitude
+//     };
+//
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//            zoom: 4,
+//            center: coordinates
+//          });
+//     // $("#map").append(map)
+//   }
+//
+// })
