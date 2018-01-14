3c526d57dc279d4e3cf6f8356936a3c4df9d4464
mjsimon91 <mjsimon91@gmail.com>
2018-01-14 09:54:25 -0500

    Finished adding the markers from FourSquare. We will likely need to figure out how to get back more responses as I only have 30 and they are all concentrated around the White House.

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
 var api_key = 'AIzaSyALhVNfaKgpvJuLqX6VuPljcwgUcEj_qHw';
-var searchLocation;
-var searchLatitude;
-var searchLongitude;
 var map;
-var fscoordinates;
-var coordinates = [
-  [38.9125591, -77.0361521],
-  [38.9125591, -77.0361521],
-  [38.9125591, -77.0361521],
-  [38.9125591, -77.0361521],
-  [38.8835686, -77.0818237],
-  [38.8981414, -77.0668551],
-  [38.8893706, -77.0200036],
-  [38.8912662, -77.0282541],
-  [38.8881601, -77.0220566],
-  [38.8977158, -77.0261858],
-];
+var fscoordinates = []; //This will hold our array of objects which is the response from FourSquare
 
+
+//Initilizing the map from Google Maps API documentation
 var map;
     function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: new google.maps.LatLng(38.8961336,-77.0028392),
-        mapTypeId: 'terrain'
 
       });
-  mapCoordinates();
+
+  //When the map is initialized, we should search FourSquare and place all the markers
+
   searchFourSquare();
 }
-
-    function mapCoordinates() {
-      for (var i = 0; i < coordinates.length; i++) {
-        var latLng = new google.maps.LatLng(coordinates[i][0],coordinates[i][1]);
-        var marker = new google.maps.Marker({
-          position: latLng,
-          map: map
-        });
-      }
-    }
 
     //Creating a request to search four square
     function searchFourSquare(){
@@ -46,6 +24,8 @@ var map;
 
       //Creating a call to moment'js in order to add to the end of the squareURL
       var now = moment().format("YYYYMMDD");
+
+      //URL endpoint for foursquare which contains the city of Washington DC hardcoded in for now
       var squareURL = 'https://api.foursquare.com/v2/venues/search?near=Washington&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=' + now;
 
       //Making a call to the url for the city in order to display the popular locations
@@ -53,15 +33,27 @@ var map;
         url: squareURL,
         method: "GET"
       }).done(function(response){
-        for (var i = 0; i < response.response.venues[i].length; i++) {
-          fscoordinates = new Object();
 
-          fscoordinates.name = response.response.venues[i].name;
-          fscoordinates.lat = response.response.venues[i].location.lat;
-          fscoordinates.lng = response.response.venues[i].location.lng;
-          fscoordinates.hereNow = response.response.venues[i].hereNow.count;
-          console.log(fscoordinates);
-      }
+        // For each of the venue responses matching the city, loop through and create an array of objects
 
-    });
-  }
+        for (var i = 0; i < response.response.venues.length; i++) {
+          fscoordinates.push({
+            name: response.response.venues[i].name,
+            lat:response.response.venues[i].location.lat,
+            lng: response.response.venues[i].location.lng,
+            hereNow:response.response.venues[i].hereNow.count
+          })
+
+
+          //Loop through all of the objects to display all markers on the map
+
+          for (var j = 0; j < fscoordinates.length; j++) {
+            var latLng = new google.maps.LatLng(fscoordinates[j].lat,fscoordinates[j].lng);
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map
+            });
+          } // Completes the loop through the array of objects
+      } // Completes the loop through add all responses to an array of objects
+    }); // Completes the function that pulls down the response
+  } // Completes the entire function that searches foursquare
