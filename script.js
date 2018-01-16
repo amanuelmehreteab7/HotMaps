53e185415fdb0a656734a236ee98c778f507eb78
Jean Tzintzun <jtzintzun@Jeans-MacBook.local>
2018-01-16 12:09:35 -0500

    sort by top10 categories in the city

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var locat = 'Washington'
 var categoryId
 var mLat = 38.8961336
 var mLgt = -77.0028392
+var buttonClicked
 
 //Initilizing the map from Google Maps API documentation
 var map;
@@ -23,15 +24,18 @@ var map;
 
     //Creating a request to search four square
     function searchFourSquare(){
-      var clientID = 'WH0QFEZQIC5FOAJTI2EIZNGUPNGVJFPCLFMDD4NLDSITUSVY';
-      var clientSecret = 'PDNYILQOD54ECFYKY2HPWNOHF3HRS4SHCSALCKEE3U1ZY3HE';
+      var clientID = 'Z3ZK4RYUU12ONLPWGOTA5DY3KOTMYFIVRFEOWW0KZ3VB13TF';
+      var clientSecret = 'JRZK5DZC5GJVEFTIOEJVEGH14KPSI5V5XUJWPD3KTYFXEQK1';
 
       //Creating a call to moment'js in order to add to the end of the squareURL
       var now = moment().format("YYYYMMDD");
 
       //URL endpoint for foursquare which contains the city of Washington DC hardcoded in for now
-      // var squareURL = 'https://api.foursquare.com/v2/venues/search?near=Washington&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=' + now;
-      var squareURL = 'https://api.foursquare.com/v2/venues/search?near='+ locat +'&categoryId=' + categoryId +'&client_id=' + clientID + '&limit=' + results + '&client_secret=' + clientSecret + '&v=' + now;
+
+            var squareURL = 'https://api.foursquare.com/v2/venues/search?near='+ locat +'&client_id=' + clientID + '&limit=50' + '&client_secret=' + clientSecret + '&v=' + now;
+
+    // Use this URL later to search by category
+    //   var squareURL = 'https://api.foursquare.com/v2/venues/search?near='+ locat +'&categoryId=' + categoryId +'&client_id=' + clientID + '&limit=' + results + '&client_secret=' + clientSecret + '&v=' + now;
 
 
       //Making a call to the url for the city in order to display the popular locations
@@ -39,6 +43,8 @@ var map;
         url: squareURL,
         method: "GET"
       }).done(function(response){
+
+        console.log(response);
 
         // For each of the venue responses matching the city, loop through and create an array of objects
         // console.log(response);
@@ -49,20 +55,16 @@ var map;
             lng: response.response.venues[i].location.lng,
             hereNow:response.response.venues[i].hereNow.count,
             id:response.response.venues[i].categories[0].id,
-            categoriy: response.response.venues[i].categories[0].pluralName
+            categoriy: response.response.venues[i].categories[0].pluralName,
+            checkinsCount: response.response.venues[i].stats.checkinsCount
           })
 
-              ////=====
               mLat = response.response.venues[0].location.lat;
-              console.log('Latitud coordinates' + mLat);
               mLgt = response.response.venues[0].location.lng;
-              console.log('Longitud coordinates' + mLgt);
                 map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 12,
                 center: new google.maps.LatLng(mLat,mLgt),
                   });
-
-              //=====
 
           //Loop through all of the objects to display all markers on the map
 
@@ -74,26 +76,61 @@ var map;
             });
           } // Completes the loop through the array of objects
       } // Completes the loop through add all responses to an array of objects
+
+      // sort object by chekin count
+
+      for (var i = 0; i < fscoordinates.length; i++) {
+        for (var j=i+1; j < fscoordinates.length; j++) {
+          if (fscoordinates[i].checkinsCount < fscoordinates[j].checkinsCount)
+          {
+            t=fscoordinates[i];
+            fscoordinates[i] = fscoordinates[j];
+            fscoordinates[j] = t
+          }
+        }
+      }
+
+      // _.orderBy(fscoordinates, ['checkinsCount', 'hereNow'], ['desc', 'desc']);
+
+      console.log("==============log Object after sort=========================");
+      console.log(fscoordinates);
+
+      // create an object with uniques category IDs
+
+      var uniqueObject = [];
+      uniqueObject = _.uniqBy(fscoordinates, 'id')
+      console.log("==============log unique Object=========================");
+      console.log(uniqueObject);
+
+      // select the top 10 categories
+      
+        uniqueObject.splice([9],uniqueObject.length - 10)
+
+      console.log('========top Ten=========');
+      console.log(uniqueObject);
+
+
     }); // Completes the function that pulls down the response
-      // console.log(fscoordinates);
 
   } // Completes the entire function that searches foursquare
 
 
-  // function search(){
+  // Click event search(){
   $('#searchCity').on('click', function(event) {
     console.log('searchCity Button - Clicked');
     event.preventDefault();
     locat = $('#search').val().trim();
-    results = $('#resultsNumber').val().trim()
-    categoryId  = $('#categories').val().trim()
     console.log('Var Location' + locat);
-    console.log('Var results' + results);
-    console.log('var categories' + categoryId);
+
+    // use later for search by category and restrict the number of results
+    // results = $('#resultsNumber').val().trim()
+    // console.log('Var results' + results);
+    // categoryId  = $('#categories').val().trim()
+    // console.log('var categories' + categoryId);
+
     fscoordinates = []
     $('#header').html('<h2>' + 'What is Happening in ' + locat + '</h2>');
     searchFourSquare()
-    // initMap()
   })
 
   $(document).ready(function() {
