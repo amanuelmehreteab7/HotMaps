efea8145c5a34ba76c7ad9258c58660617d6cb47
Amanuel <amanuel@Amanuels-MacBook-Air.local>
2018-01-20 10:06:13 -0500

    twitter theme

--- twitter.js
+++ twitter.js
@@ -14,8 +14,9 @@ updateTwitterTimeline = (profile) => {
 
   tTag.attr('href', `https://twitter.com/${profile}`)
   tTag.attr('class', 'twitter-timeline')
-  tTag.attr('data-width', '300')
+  tTag.attr('data-width', 'auto')
   tTag.attr('data-height', '500')
+  tTag.attr('theme', 'light')
 
   sidePanel.append(tTag)
 
