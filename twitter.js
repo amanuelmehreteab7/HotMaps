3ac3e9246a947b2e969e9c5376e8e8e670ceb033
Amanuel <amanuel@Amanuels-MacBook-Air.local>
2018-01-20 09:45:27 -0500

    twitter clears when clicking another marker

--- twitter.js
+++ twitter.js
@@ -9,12 +9,13 @@ var sidePanel = $('#twitter');
 
 updateTwitterTimeline = (profile) => {
 
+  sidePanel.empty();
   var tTag= $('<a>');
 
   tTag.attr('href', `https://twitter.com/${profile}`)
   tTag.attr('class', 'twitter-timeline')
   tTag.attr('data-width', '300')
-  tTag.attr('data-height', '300')
+  tTag.attr('data-height', '500')
 
   sidePanel.append(tTag)
 
